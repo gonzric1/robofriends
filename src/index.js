436d46a9a86a9f1e34178c591254d6bacae70dc8
Ricky Gonzalez <rickygonzalez@gmail.com>
2020-02-01 01:35:32 -0600

    deleted uneeded files

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
-import Hello from './Hello';
-import 'tachyons'
+import Card from './Card'
+import 'tachyons';
 
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Hello />, document.getElementById('root'));
+ReactDOM.render(<Card />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
