d007c8ebf94c6fe2c7748d33f0a5a104fe0b8966
Ricky Gonzalez <rickygonzalez@gmail.com>
2020-02-03 01:58:35 -0600

    finished converting from state to redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'; 
+
+import { createLogger } from 'redux-logger';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import * as serviceWorker from './serviceWorker';
+import thunkMiddleware from 'redux-thunk';
+
 import './index.css';
 import App from './App';
 import 'tachyons';
+import { searchRobots, requestRobots } from './reducers';
 
-import * as serviceWorker from './serviceWorker';
+const logger = createLogger();
+const rootReducer = combineReducers({searchRobots, requestRobots})
+
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger) )
 
 
 ReactDOM.render(
-    <App />
+    <Provider store = { store }>
+        <App />
+    </Provider>
 , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
